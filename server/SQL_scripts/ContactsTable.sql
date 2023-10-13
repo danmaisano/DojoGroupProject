23a243f7311dea9bb7e3f40b8eea94e76427da9f
Ryan Harvey <124230161+Harvey-Ryan@users.noreply.github.com>
2023-10-13 11:14:09 -0500

    Create ContactsTable.sql Script
    Initial mapping of necessary data for Contacts implementation.

---
+++ server/SQL_scripts/ContactsTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS
+    "contacts" (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        cell_phone VARCHAR(20),
+        work_phone VARCHAR(20) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        company_id INTEGER REFERENCES "companies"(id),
+        company_title VARCHAR(255) NOT NULL,
+        opportunity_id INTEGER REFERENCES "opportunities"(id),
+        notes VARCHAR(255),
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP
+    );
