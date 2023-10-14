4fa52d086539e096f18c221c66ae7594f3264131
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-10-13 22:38:11 -0500

    First itteration of Contact Routes

---
+++ server/routes/contact.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+const router = express.Router();
+import contactController from "../controllers/contact.controller.js";
+import checkPermission from "../middleware/checkPermission.js";
+import verifyUser from "../middleware/verifyUser.js";
+
+router.get("/user/:id", verifyUser, checkPermission('readOwn', 'contact'), contactController.getAllContacts);
+router.get("/company/:id", verifyUser, checkPermission('readOwn', 'contact'), contactController.getAllContactsByCompany);
+router.get("/company/:id/opportunity/:opportunity_id", verifyUser, checkPermission('readOwn', 'contact'), contactController.getAllContactsByCompanyAndOpportunity);
+router.get("/:id", verifyUser, checkPermission('readOwn', 'contact'), contactController.getContact);
+router.put("/:id", verifyUser, checkPermission('updateOwn', 'contact'), contactController.updateContact);
+router.delete("/:id", verifyUser, checkPermission('deleteOwn', 'contact'), contactController.deleteContact);
+router.post("/create", verifyUser, checkPermission('createOwn', 'contact'), contactController.createContact);
+
+export default router;
