72f3042d66595c260d0502ff43861d04b6290bb4
Logan Pendergraft <ldpendergraft@gmail.com>
2023-10-15 12:37:18 -0500

    Added: react-bootstrap-icons package Top Nav, Side Nav, and Header Corrected routes to only show nav layout once a user is logged in. Added div to make table responsive to device size.  Corrected App Name on Home page  Added Import to main.jsx for bootstrap-icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-import Layout from './components/Layout'
-import { useState, useEffect   } from 'react'
-import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom'
-import Home from './Home'
-import Register from './Register'
-import Login from './Login'
-import NewOpportunityForm from './components/NewOpp'
-import Dashboard from './Dashboard'
-import Cookies from 'js-cookie'
-import CompanyDetails from './CompanyDetails'
-// import ContactCard from './components/ContactCard'
-
-
+import Layout from './components/Layout';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import Home from './Home';
+import Register from './Register';
+import Login from './Login';
+import NewOpportunityForm from './components/NewOpp';
+import Dashboard from './Dashboard';
+import Cookies from 'js-cookie';
+import CompanyDetails from './CompanyDetails';
+// import ContactCard from './components/ContactCard';
 
 function App() {
   const [user, setUser] = useState({});
@@ -22,23 +20,25 @@ function App() {
       setUser(userData);
     }
   }, []);
-  
+
   return (
     <BrowserRouter>
-      <Layout user={user} setUser={setUser}>
-        <Routes>
-          <Route path='/' element={<Home user={user} setUser={setUser}/>}></Route>
-          <Route path='/register' element={<Register user={user} setUser={setUser}/>}></Route>
-          <Route path='/login' element={<Login user={user} setUser={setUser}/>}></Route>
-          <Route path='/dashboard' element={<Dashboard user={user} setUser={setUser}/>}></Route>
-          <Route path='/company/:id' element={<CompanyDetails user={user} setUser={setUser}/>}></Route>
-          <Route path='/newOpp' element={<NewOpportunityForm user={user} setUser={setUser}/>}></Route>
-          {/* <Route path='/test' element={<ContactCard user={user} setUser={setUser}/>}></Route> */}
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home user={user} setUser={setUser} />} />
+        <Route path="/register" element={<Register user={user} setUser={setUser} />} />
+        <Route path="/login" element={<Login user={user} setUser={setUser} />} />
+        
+        {/* Layout wraps the routes below */}
+        <Route element={<Layout user={user} setUser={setUser} />}>
+          <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser} />} />
+          <Route path="/company/:id" element={<CompanyDetails user={user} setUser={setUser} />} />
+          <Route path="/newOpp" element={<NewOpportunityForm user={user} setUser={setUser} />} />
+          {/* <Route path="/test" element={<ContactCard user={user} setUser={setUser} />} /> */}
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-
 export default App;
+
