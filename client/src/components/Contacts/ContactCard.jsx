81f6377a01620b651c314e72d6bedbaae7b7358f
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-10-16 23:42:46 -0500

    Added Edit logic to ContactCard: - Dashboard refreshes to show updated data when ContactCard is closed

--- client/src/components/Contacts/ContactCard.jsx
+++ client/src/components/Contacts/ContactCard.jsx
@@ -17,30 +17,16 @@ const ContactModal = ({ contact }) => {
     console.log("contact: ", contact);
 
     const [editMode, setEditMode] = React.useState(false);
+    const [contactEditMode, setContactEditMode] = React.useState(false);
     const [showContactModal, setShowContactModal] = React.useState(true);
     const [pendingOpps, setPendingOpps] = React.useState([]);
-    const [wonOpps, setWonOpps] = React.useState([])
-    // const [contactDetails, setContactDetails] = React.useState(null);
+    const [wonOpps, setWonOpps] = React.useState([]);
+    const [notes, setNotes] = React.useState(contact.notes);
+    const [updatedContact, setUpdatedContact] = React.useState(contact);
+    const [thisContact, setThisContact] = React.useState(contact);
+    const [formData, setFormData] = React.useState({ ...contact });
 
-    // useEffect(() => {
-    //     if (contact) {
-    //         // Fetch contact details by ID from the server
-    //         axios.get(`http://localhost:8081/opportunities/contact/${contact.id}`, {
-    //             withCredentials: true
-    //         })
-    //             .then((response) => {
-    //                 setContactDetails(response.data);
-    //             })
-    //             .catch((error) => {
-    //                 console.error(error);
-    //             });
-    //     }
-    // }, [contact]);
-
-    // if (!contactDetails) {
-    //     return null; // Render loading or handle error
-    // }
-
+    // Get and filter Opportunities associated with Contact
     const getContactOpportunities = async (contactId) => {
         try {
             const response = await axios.get(`http://localhost:8081/opportunities/contact/${contactId}`, {
@@ -59,42 +45,90 @@ const ContactModal = ({ contact }) => {
         }
     };
 
+    //Get contact by id
+    const getContactById = async (contactId) => {
+        try {
+            const response = await axios.get(`http://localhost:8081/contacts/${contactId}`, {
+                withCredentials: true
+            });
+            console.log(response.data);
+            const contact = response.data;
+            setNotes(contact.notes);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // Call the above function when the component mounts.
     useEffect(() => {
         if (contact) {
             getContactOpportunities(contact.id);
+            setUpdatedContact(getContactById(contact.id));
         }
     }, [contact]);
 
-    // const getWonOpportunities = async (contactId) => {
-    //     const response = await axios.get(`http://your-api-url/opportunities?contact_id=${contactId}&status=Won`);
-    //     setWonOpps(response.data);
-    // }
-
-    // Get Contact by Id
-    const getContact = async (id) => {
-        const response = await contact.findById(id);
-        setContact(response);
-    };
-
-    // Update Contact
-    const updateContact = async () => {
-        const response = await contact.findByIdAndUpdate(
-            contact.id,
-            contact,
-            { new: true }
-        );
-        setContact(response);
-        setEditMode(false);
-    };
-
     // Close Modal
-    const handleClose = () => {
+    const closeModal = () => {
         setShowContactModal(false);
+        window.location.reload();
     };
 
     // Open default email client and prefil address
     const sendEmail = () => {
         window.location.href = 'mailto:johndoe@companyname.com';
+    };
+
+    // Edit Contact Info
+    const handleContactEditMode = () => {
+        setUpdatedContact(thisContact);
+        setContactEditMode(true);
+    };
+
+    const handleContactChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const saveContact = () => {
+        axios
+            .put(`http://localhost:8081/contacts/${contact.id}`, formData, {
+                withCredentials: true
+            })
+            .then((response) => {
+                console.log("Contact updated successfully: ", response.data);
+                setThisContact(formData);
+                setContactEditMode(false);
+            })
+            .catch((error) => {
+                console.error("Error updating contact:", error);
+            });
+    };
+
+    // Edit notes
+    const handleEditMode = () => {
+        setEditMode(true);
+        setNotes(contact.notes);
+    };
+
+    const handleNotesChange = (e) => {
+        setNotes(e.target.value);
+    };
+
+    const handleSaveNotes = () => {
+        // Save the updated notes to the DB
+        axios
+            .put(`http://localhost:8081/contacts/${contact.id}`, { notes }, {
+                withCredentials: true
+            })
+            .then((response) => {
+                console.log('Notes updated successfully:', response.data);
+                const updatedContact = { ...contact, notes: notes };
+                // Store updated contact in state
+                setThisContact(updatedContact);
+                setEditMode(false);
+            })
+            .catch((error) => {
+                console.error('Error updating notes:', error);
+            });
     };
 
     function formatPhoneNumber(phoneNumberString) {
@@ -108,18 +142,63 @@ const ContactModal = ({ contact }) => {
 
     return (
         <div className='m-5'>
-            <Modal show={showContactModal} onHide={handleClose}>
-                {/* TODO: Populate information with DB query ----- <= TODO  */}
+            <Modal show={showContactModal} onHide={closeModal}>
                 <Modal.Header closeButton>
                     Contact Details:
                 </Modal.Header>
                 <Modal.Body className='p-4'>
                     <div className='d-flex justify-content-between'>
                         <div>
-                            <h3>{`${contact.first_name} ${contact.last_name}`}</h3>
-                            <p className='mb-0'><img src={cellphone} className='m-1' /> <a href='#'>{formatPhoneNumber(contact.cell_phone)}</a></p>
-                            <p><img src={phone} className='m-1' /> <a href='#'>{formatPhoneNumber(contact.work_phone)}</a></p>
-                            <p><img src={email} className='m-1' /><a href={`mailto:${contact.email}`}>{contact.email}</a></p>
+                            {contactEditMode ? (
+                                <>
+                                    <input
+                                        type="text"
+                                        id='first_name'
+                                        name='first_name'
+                                        value={formData.first_name}
+                                        onChange={handleContactChange}
+                                    />
+                                    <input
+                                        type="text"
+                                        id='last_name'
+                                        name='last_name'
+                                        value={formData.last_name}
+                                        onChange={handleContactChange}
+                                    />
+                                    <input
+                                        type="text"
+                                        id='cell_phone'
+                                        name='cell_phone'
+                                        value={formData.cell_phone}
+                                        onChange={handleContactChange}
+                                    />
+                                    <input
+                                        type="text"
+                                        id='work_phone'
+                                        name='work_phone'
+                                        value={formData.work_phone}
+                                        onChange={handleContactChange}
+                                    />
+                                    <input
+                                        type="text"
+                                        id='email'
+                                        name='email'
+                                        value={formData.email}
+                                        onChange={handleContactChange}
+                                    />
+                                    <button onClick={saveContact}>Save</button>
+                                </>
+                            ) : (
+                                <>
+                                    <div className='d-flex justify-content-between'>
+                                        <h3>{`${thisContact.first_name} ${thisContact.last_name}`}</h3>
+                                        <img src={edit} className='m-1' onClick={() => setContactEditMode(true)} />
+                                    </div>
+                                    <p className='mb-0'><img src={cellphone} className='m-1' /> <a href='#'>{formatPhoneNumber(thisContact.cell_phone)}</a></p>
+                                    <p><img src={phone} className='m-1' /> <a href='#'>{formatPhoneNumber(thisContact.work_phone)}</a></p>
+                                    <p><img src={email} className='m-1' /><a href={`mailto:${thisContact.email}`}>{thisContact.email}</a></p>
+                                </>
+                            )}
                         </div>
                         <div className='m-auto w-50'>
                             <div className='d-flex flex-column m-auto w-75'>
@@ -136,22 +215,20 @@ const ContactModal = ({ contact }) => {
                                             )))
                                         }
                                     </div>
-                                    {/* </div> */}
                                 </div>
                                 <div className='d-flex flex-column m-autow-75'>
                                     <div className='d-flex flex-column p-2'>
                                         <div className='justify-content-between d-flex flex-column'>
                                             <h5>Secured:</h5>
-                                            {/* <a href='#'>More</a> */}
                                             {wonOpps.length === 0 ? (
-                                            <p>No Business History</p>
-                                        ) : (
-                                            wonOpps.map((opportunity) => (
-                                                <div className='d-flex flex-column' key={opportunity.id}>
-                                                    <a href="#">{opportunity.opportunity_name}</a>
-                                                </div>
-                                            )))
-                                        }
+                                                <p>No Business History</p>
+                                            ) : (
+                                                wonOpps.map((opportunity) => (
+                                                    <div className='d-flex flex-column' key={opportunity.id}>
+                                                        <a href="#">{opportunity.opportunity_name}</a>
+                                                    </div>
+                                                )))
+                                            }
                                         </div>
                                     </div>
                                 </div>
@@ -159,15 +236,26 @@ const ContactModal = ({ contact }) => {
                         </div>
                     </div>
                     <div>
-                        {/* TODO: Creat onClick for Edit icon that replaces <p>    ----- <= TODO*/}
-                        {/* with text area followed by a PUT/PATCH */}
-                        <div className='d-flex justify-content-between w-100'>
+                        <div className='d-flex justify-content-between w-100 border-bottom mb-2 pb-2 border-light'>
                             <h5>Notes:</h5>
-                            <img src={edit} className='m-1' />
+                            <img src={edit} className='m-1' onClick={handleEditMode} />
                         </div>
-                        <p className='border-top mt-2 border-light p-2'>
-                            {contact.notes}
-                        </p>
+                        {editMode ? (
+                            <>
+                                <input
+                                    type="text"
+                                    value={notes}
+                                    onChange={handleNotesChange}
+                                />
+                                <button onClick={handleSaveNotes}>Save</button>
+                            </>
+                        ) : (
+                            <>
+                                <p className='p-2'>
+                                    {thisContact.notes}
+                                </p>
+                            </>
+                        )}
                     </div>
                 </Modal.Body>
             </Modal>
